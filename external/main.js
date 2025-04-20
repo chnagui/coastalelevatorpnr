acd180f4c172e5e3365635d84b51a146223382e1
chnagui <75308610+chnagui@users.noreply.github.com>
2025-04-20 08:29:33 +0530

    updating the prompt in js

--- external/main.js
+++ external/main.js
@@ -1,9 +1,9 @@
-// let person = prompt("Please enter your name", "Eg: Monu");
+let person = prompt("Please enter your name", "Eg: Monu");
 
-// if (person != null) {
-//   document.getElementById("demo").innerHTML =
-//   "Hello " + person + "! How are you today?";
-// }
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
 //Marquee-----
 const marquee = document.getElementById('welcomeMarquee');
     marquee.addEventListener('mouseover', () => marquee.stop());
